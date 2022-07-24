43250c9fd780c5f442151f47f3c3e6312bda2034
liyu_sup <1035810496@qq.com>
2022-07-24 23:58:28 +0800

    项目骨架搭建完成（前后端交互完成）

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,6 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+ 
 const routes = [
   {
     path: '/',
